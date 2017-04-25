2241d50f1df11aa4f54255b898a6706285fc6baf
niel <niel.heo@gmail.com>
2017-04-25 07:06:11 +0700

    Add Client Test for auth

--- GlownyShop.Auth/Startup.cs
+++ GlownyShop.Auth/Startup.cs
@@ -17,7 +17,8 @@ namespace GlownyShop.Auth
             services.AddIdentityServer()
                 .AddTemporarySigningCredential()
                 .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryClients(Config.GetClients());
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- GlownyShop.Tests/GlownyShop.Tests.Trying/Program.cs
+++ GlownyShop.Tests/GlownyShop.Tests.Trying/Program.cs
@@ -1,11 +1,12 @@
-﻿using GlownyShop.Core.Logic;
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Net.Http;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace GlownyShop.Tests.Trying
 {
-    class Program
+    internal class Program
     {
         private static string getSalt()
         {
@@ -18,7 +19,7 @@ namespace GlownyShop.Tests.Trying
             }
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             //using (var sha256 = SHA256.Create())
             //{
@@ -28,7 +29,7 @@ namespace GlownyShop.Tests.Trying
             //    // Get the hashed string.
             //    var hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
 
-            //    // Print the string. 
+            //    // Print the string.
             //    Console.WriteLine(hash);
 
             //}
@@ -58,10 +59,53 @@ namespace GlownyShop.Tests.Trying
             //Console.WriteLine(string.Format("{0}|{1}", a, a1));
             //Console.WriteLine(SecurityService.CompareHashedPassword(a, a1));
 
-            for (int i = 0; i < 4; i++)
+            var disco = DiscoveryClient.GetAsync("http://localhost:51000").Result;
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("api1").Result;
+
+            if (tokenResponse.IsError)
             {
-                Console.WriteLine(Guid.NewGuid().ToString());
+                Console.WriteLine(tokenResponse.Error);
+                Console.Read();
+                return;
             }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = client.GetAsync("http://localhost:5555/identity").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine(Guid.NewGuid().ToString());
+            //}
+
+            // request token
+            tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            tokenResponse = tokenClient.RequestResourceOwnerPasswordAsync("alice", "password", "api1").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
 
             Console.Read();
         }
