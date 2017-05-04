87f7dc2bb70a350b92eb83ba99ac317715be6414
niel <niel.heo@gmail.com>
2017-05-04 21:08:36 +0700

    refactor Users component

--- FrontEnd/glowny-shop-admin/src/components/Users/index.js
+++ FrontEnd/glowny-shop-admin/src/components/Users/index.js
@@ -2,20 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { getGraph } from '../../actions/graphql.js'
 
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table'
-import Toggle from 'material-ui/Toggle'
+import UserList from './UserList'
 
 class Users extends React.Component {
   componentDidMount = () => {
     this.props.dispatch(getGraph(`
-      viewer { 
+      viewer {  
         adminUsers { 
           id 
           firstName 
@@ -27,8 +19,10 @@ class Users extends React.Component {
             name
           }
         }
-      }`));
+      }`)
+    );
   }
+
   render() {
     //let dispatch = this.props.dispatch
     //let fetchInProgress = String(this.props.store.get('fetching'));
@@ -38,32 +32,8 @@ class Users extends React.Component {
     const { adminUsers } = this.props.store.data
     //console.log(adminUsers)
     return (
-      <Table fixedHeader={true} selectable={false}>
-        <TableHeader displaySelectAll={false}
-            adjustForCheckbox={false}>
-          <TableRow>
-            <TableHeaderColumn>First Name</TableHeaderColumn>
-            <TableHeaderColumn>Last Name</TableHeaderColumn>
-            <TableHeaderColumn>Email</TableHeaderColumn>
-            <TableHeaderColumn>Role(s)</TableHeaderColumn>
-            <TableHeaderColumn>Active</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody displayRowCheckbox={false}>
-          {
-            adminUsers && adminUsers.map(user => 
-              <TableRow key={user.id}>
-                <TableRowColumn>{user.firstName}</TableRowColumn>
-                <TableRowColumn>{user.lastName}</TableRowColumn>
-                <TableRowColumn>{user.email}</TableRowColumn>
-                <TableRowColumn>{user.adminRoles.map(role => role.name).join('; ')}</TableRowColumn>
-                <TableRowColumn><Toggle defaultToggled={user.isActive}/></TableRowColumn>
-              </TableRow>)
-          }
-          
-        </TableBody>
-      </Table>
-    );
+      <UserList adminUsers={adminUsers} />
+    )
   }
 }
 
